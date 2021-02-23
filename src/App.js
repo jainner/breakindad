3e29f1575ea09264c95f0ee30392228682553fdc
jaiinner <jaiinner22@gmail.com>
2021-02-22 19:33:21 -0500

    proyecto finalizado

--- src/App.js
+++ src/App.js
@@ -1,25 +1,51 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import styled from '@emotion/styled'
+import Frase from './component/frase'
+
+const Contenedor = styled.div`
+	display: flex;
+	align-items:center;
+	padding-top:5rem ;
+	flex-direction:column;
+
+`
+
+const Boton = styled.button`
+	background:-webkit-linear-gradient(top left,
+		#007d35 0%, #007d35 40%, #0f574e 100%
+	);
+	background-size: 300px;
+	font-family:Arial,Helvetica,'sans-serif';
+	color:#fff;
+	margin-top:3rem;
+	padding:1rem 3rem;
+	font-size:2rem;
+	border:none;
+	box-shadow: 3px 3px 5px 3px #000000;
+	cursor:pointer;
+`
 
 function App() {
+
+	const [frase,guargdarFrase] = useState({})
+
+	const consultarApi = async() =>{
+		const api = await fetch('https://breaking-bad-quotes.herokuapp.com/v1/quotes')
+		const frase = await api.json()
+		guargdarFrase(frase[0])		
+	}
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+	<Contenedor>
+		<Frase
+			frase={frase}
+		/>
+		<Boton
+			onClick={consultarApi}
+		>
+			obtener frase
+		</Boton>
+	</Contenedor>
   );
 }
 
